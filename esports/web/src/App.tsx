8d12c43e231910e3ede3bae7a6d57ef3385b6f7d
Maiconsilvacarvalho <maiquinhomaicon@hotmail.com>
2022-09-17 12:02:19 -0300

    Component Creation to Isolate Game and Create New Games

--- esports/web/src/App.tsx
+++ esports/web/src/App.tsx
@@ -1,11 +1,13 @@
 //  JSX - Javascript + XML
 // componente e propriueade
 import { MagnifyingGlassPlus } from "phosphor-react";
-
+import { GameBanner } from "./components/GameBanner"
 import './styles/main.css';
 
 
 import logoImg from './assets/logo-nlw-esports.svg';
+import { CreateAdBanner } from "./components/CreateAdBanner";
+;
 
 function App() {
   return (
@@ -18,17 +20,15 @@ function App() {
   
   <div className="grid grid-cols-6 gap-6 mt-16">
     
-    <a href="" className="relative rounded-lg overflow-hidden">
-      <img src="/game-1.png" alt="" />
-
-      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-      <strong className="font-bold text-white block" >League of Legends</strong>
-      <span className="text-zinc-300 text-sm-14 block "> 4 anúncios</span>
-    </div>
-    </a>
+  <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5}/>
+  <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={5}/>
+  <GameBanner bannerUrl="/game-3.png" title="CS:GO" adsCount={5}/>
+  <GameBanner bannerUrl="/game-4.png" title="Apex Legends" adsCount={5}/>
+  <GameBanner bannerUrl="/game-7.png" title="World " adsCount={5}/>
+  <GameBanner bannerUrl="/game-6.png" title="League of Legends" adsCount={5}/>
 
     
-    <a href="" className="relative rounded-lg overflow-hidden">
+{/*      <a href="" className="relative rounded-lg overflow-hidden">
       <img src="/game-6.png" alt="" />
 
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
@@ -36,7 +36,8 @@ function App() {
       <span className="text-zinc-300 text-sm-14 block "> 4 anúncios</span>
     </div>
     </a>
-    <a href="" className="relativ rounded-lg overflow-hidden">
+
+    <a href="" className="relative rounded-lg overflow-hidden">
       <img src="/game-3.png" alt="" />
       
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
@@ -67,25 +68,10 @@ function App() {
       <strong className="font-bold text-white block" >Dota 2</strong>
       <span className="text-zinc-300 text-sm-14 block "> 4 anúncios</span>
     </div>
-    </a>
+    </a>  */}
   </div>  
 
-    <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8  overflow-hidden">
-
-    <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-
-        <div>
-          <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
-          <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
-          
-          </div>
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24}/>
-            Publicar anúncio
-          </button>
-        
-      </div>
-    </div>
+    <CreateAdBanner/>
 
   </div>
 
