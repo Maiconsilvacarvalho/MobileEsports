ea39958a471e8209d155e0e3a0f3637944f23cba
Maiconsilvacarvalho <maiquinhomaicon@hotmail.com>
2022-09-17 19:14:05 -0300

    update in the search for information through API

--- esports/mobile/src/components/GameCard/Index.tsx
+++ esports/mobile/src/components/GameCard/Index.tsx
@@ -5,12 +5,14 @@ import  {LinearGradient}  from "expo-linear-gradient";
 import { styles } from './styles';
 import { THEME } from '../../theme';
 
-
+// mostrar o que vai trazer na interfe através da API
 export interface GameCardProps{
     id: string,
-    name: string,
-    ads: string,
-    cover: ImageSourcePropType;
+    title: string,
+    _count:{
+      ads: number
+    };
+    bannerUrl: string;
 }
 
 interface Props  extends TouchableOpacityProps{
@@ -25,22 +27,22 @@ export function GameCard({data, ...rest}: Props) {
 
     <ImageBackground
         style={styles.cover}
-        source={data.cover}
+        source={{uri: data.bannerUrl}}// trazendo a URL da imagem 
     >
 
 
 <LinearGradient
         // Background Linear Gradient
-        colors={[THEME.COLORS.FOOTER]}
+        colors={THEME.COLORS.FOOTER}
         style={styles.footer}
       >
       
         <Text style={styles.name}>
-            {data.name}
+            {data.title} {/* trazendo title da API */}
         </Text>
 
         <Text style={styles.ads}>
-            {data.ads} anúncios
+            {data._count.ads} anúncios {/* trazendo anúncios da API */}
         </Text>
       </LinearGradient>
 
--- esports/mobile/src/screens/home/index.tsx
+++ esports/mobile/src/screens/home/index.tsx
@@ -1,14 +1,26 @@
 import { View, Image, FlatList } from 'react-native';
-
+import {useEffect, useState} from 'react';
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard/Index';
+import { GameCard, GameCardProps } from '../../components/GameCard/Index';
 import { Heading } from '../../components/Heading';
-import { GAMES } from "../../utils/games";
+
 import { styles } from './styles';
 
 
 
 export function Home() {
+//consumo da api via IP do computador
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() =>{
+    fetch('http://192.168.15.9:3333/games')
+    .then(response => response.json())//converter resposta paa json
+    .then(data => setGames(data))
+  },[]);
+
+
+
+
   return (
     <View style={styles.container}>
 
@@ -23,7 +35,7 @@ export function Home() {
       />
 
         <FlatList
-          data={GAMES} //lista os games
+          data={games} //lista os games
           keyExtractor={item => item.id}// tras os itens respeitando o ID, 1,2,3 ETC..
           renderItem={({item}) =>(
             <GameCard 
