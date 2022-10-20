63d0fd670bc8f46ae115ae9febb29dd235ad2848
Maicon <33131590+Maiconsilvacarvalho@users.noreply.github.com>
2022-10-20 10:57:30 -0300

    att gamer

--- esports/web/src/components/GameBanner.tsx
+++ esports/web/src/components/GameBanner.tsx
@@ -7,7 +7,7 @@ interface GameBannerProps{
 
 export function GameBanner (props: GameBannerProps) {
 
-    return (
+    return(
         <a href="" className="relative rounded-lg overflow-hidden">
         <img src={props.bannerUrl} alt="" />
   
